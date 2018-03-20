08ec3286070580689076036f1e165261181ce410
Dongzhen <wyatt_h@outlook.com>
2018-03-20 21:02:10 +0100

    validate the sort and add functions

--- code/data/validation/sort_and_add/draw.m
+++ code/data/validation/sort_and_add/draw.m
@@ -1,16 +1,18 @@
 %% define parameters
 % everytime remember to change the first three parameters
+close all
 clear
 clc
-generation = 1;
+generation = 9;
+number = 3;
 str_old = ['data',num2str(generation)];
 str_new = ['data',num2str(generation +1)];
 load([str_old '_desire.mat'])
 oldlefts = left;
 oldrights = right;
 direction = enddirection(size(enddirection,1));
-numberleft = 2;
-numberright = 2;
+numberleft = number;
+numberright = number;
 maxdistance = 5;
 mindistance = 3;
 maxangle = 0.125*pi;
@@ -29,7 +31,6 @@ enddirection = [enddirection, newdirection];
 save([str_new '_desire.mat'],'left','right', 'position','enddirection')
 show([str_new '_desire'],'-')
 figure()
-
 load([str_old '.mat'])
 newleft = chaos(newleft);
 newright = chaos(newright);
@@ -37,3 +38,5 @@ left = [left; newleft];
 right = [right; newright];
 save([str_new '.mat'],'left','right', 'position')
 show(str_new,'')
+saveas(1,['pictures/' str_new '_desire.jpg'])
+saveas(2,['pictures/' str_new '_input.jpg'])
